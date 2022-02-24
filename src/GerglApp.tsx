109e877f239425099c6ff0b94f0df22e0115d4a0
Greg Foltz <gfoltz@gmail.com>
2022-02-24 05:55:08 +0000

    Calculate height to fit screen

--- src/GerglApp.tsx
+++ src/GerglApp.tsx
@@ -10,45 +10,46 @@ const Container = styled.div((props: any) => ({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  width:"100%",
-  height:'794px',
+  width: "100%",
+  height: "calc(100% - 50px)",
 }));
 
 const PageHeader = styled.div({
-  fontSize:"37px",
+  fontSize: "37px",
   fontWeight: 700,
   textAlign: "center",
   borderBottomStyle: "solid",
   borderBottomColor: 'lightgrey',
   paddingLeft: "16px",
   paddingRight: "16px",
+  height: '50px',
 });
 
-export function GerglApp(props:any) {
+export function GerglApp(props: any) {
   const stateMachine = useGerglStateMachine();
-  const {state, mutations} = stateMachine;
-  
+  const { state, mutations } = stateMachine;
+
   return (
     <GerglStateContext.Provider value={stateMachine}>
-        <KeyboardEventHandler
-            handleKeys={['alphabetic', 'backspace', 'enter']}
-            onKeyEvent={(key: string) => {
-                if (key === 'enter') {
-                    mutations.makeGuess(state);
-                } else if (key === 'backspace') {
-                    mutations.removeLetter(state);
-                } else {
-                    mutations.addLetter(state, key.toLocaleUpperCase());
-                }
-            }} />
-                
-        <PageHeader>
-            GERGL
-        </PageHeader>
-        <Container>
-            <Board state={state} />
-            <Keyboard/>
-        </Container>
+      <KeyboardEventHandler
+        handleKeys={['alphabetic', 'backspace', 'enter']}
+        onKeyEvent={(key: string) => {
+          if (key === 'enter') {
+            mutations.makeGuess(state);
+          } else if (key === 'backspace') {
+            mutations.removeLetter(state);
+          } else {
+            mutations.addLetter(state, key.toLocaleUpperCase());
+          }
+        }} />
+
+      <PageHeader>
+        GERGL
+      </PageHeader>
+      <Container>
+        <Board state={state} />
+        <Keyboard />
+      </Container>
     </GerglStateContext.Provider>
   );
 }
--- src/GerglState.tsx
+++ src/GerglState.tsx
@@ -1,111 +1,111 @@
 import React, { useContext, useState } from "react"
 
 export type GerglState = {
-    secret: string,
-    guesses: string[],
-    currentGuess: string,
+  secret: string,
+  guesses: string[],
+  currentGuess: string,
 }
 type SetStateFn = React.Dispatch<React.SetStateAction<GerglState>>;
 
 export interface GerglStateMachine {
-    readonly state: GerglState;
-    
-    readonly mutations: {
-        addLetter(state: GerglState, letter: string): GerglState;
-        removeLetter(state: GerglState): GerglState;
-        makeGuess(state: GerglState): GerglState;
-    }
+  readonly state: GerglState;
 
-    readonly selectors: {
-        isGameComplete(state: GerglState): boolean;
-    };
+  readonly mutations: {
+    addLetter(state: GerglState, letter: string): GerglState;
+    removeLetter(state: GerglState): GerglState;
+    makeGuess(state: GerglState): GerglState;
+  }
+
+  readonly selectors: {
+    isGameComplete(state: GerglState): boolean;
+  };
 }
 
 export function newGerglState(): GerglState {
-    return {
-        secret: "GERGL",
-        guesses: [],
-        currentGuess: '',
-    }
+  return {
+    secret: "GERGL",
+    guesses: [],
+    currentGuess: '',
+  }
 }
 
 export function useGerglStateMachine(): GerglStateMachine {
-    const [state, setState] = useState<GerglState>(newGerglState)
-    return newGerglStateMachine(state, setState);
+  const [state, setState] = useState<GerglState>(newGerglState)
+  return newGerglStateMachine(state, setState);
 }
 
 function newGerglStateMachine(state: GerglState, setState: SetStateFn) {
-    return {
-        state,
-        mutations: {
-            addLetter: addLetterMutation(setState),
-            removeLetter: removeLetterMutation(setState),
-            makeGuess: makeGuessMutation(setState),
-        },
-        selectors: {
-            isGameComplete,
-        }
+  return {
+    state,
+    mutations: {
+      addLetter: addLetterMutation(setState),
+      removeLetter: removeLetterMutation(setState),
+      makeGuess: makeGuessMutation(setState),
+    },
+    selectors: {
+      isGameComplete,
     }
+  }
 }
 
 function addLetterMutation(setState: SetStateFn) {
-    return (state: GerglState, letter: string): GerglState => {
-        return mutation(setState, () => {
-            if (state.currentGuess.length >= 5 || isGameComplete(state)) {
-                return state;
-            }
-            return {
-                ...state,
-                currentGuess: state.currentGuess + letter,
-            }
-        });
-    };
+  return (state: GerglState, letter: string): GerglState => {
+    return mutation(setState, () => {
+      if (state.currentGuess.length >= 5 || isGameComplete(state)) {
+        return state;
+      }
+      return {
+        ...state,
+        currentGuess: state.currentGuess + letter,
+      }
+    });
+  };
 }
 
 function removeLetterMutation(setState: SetStateFn) {
-    return (state: GerglState): GerglState => {
-        return mutation(setState, () => {
-            if (state.currentGuess.length <= 0 || isGameComplete(state)) {
-                return state;
-            }
-            return {
-                ...state,
-                currentGuess: state.currentGuess.slice(0, -1),
-            }
-        });
-    };
+  return (state: GerglState): GerglState => {
+    return mutation(setState, () => {
+      if (state.currentGuess.length <= 0 || isGameComplete(state)) {
+        return state;
+      }
+      return {
+        ...state,
+        currentGuess: state.currentGuess.slice(0, -1),
+      }
+    });
+  };
 }
 
 function makeGuessMutation(setState: SetStateFn) {
-    return (state: GerglState): GerglState => {
-        return mutation(setState, () => {
-            if (state.currentGuess.length !== 5 || isGameComplete(state)) {
-                return state;
-            }
-            return {
-                ...state,
-                guesses: [...state.guesses, state.currentGuess],
-                currentGuess: '',
-            }
-        });
-    };
+  return (state: GerglState): GerglState => {
+    return mutation(setState, () => {
+      if (state.currentGuess.length !== 5 || isGameComplete(state)) {
+        return state;
+      }
+      return {
+        ...state,
+        guesses: [...state.guesses, state.currentGuess],
+        currentGuess: '',
+      }
+    });
+  };
 }
 
 function isGameComplete(state: GerglState) {
-    return (state.guesses.length !== 0 && (state.guesses[state.guesses.length - 1] === state.secret)) 
-        || (state.guesses.length >= 6 );
+  return (state.guesses.length !== 0 && (state.guesses[state.guesses.length - 1] === state.secret))
+    || (state.guesses.length >= 6);
 }
 
 function mutation(setState: SetStateFn, mutator: () => GerglState) {
-    const newState = mutator();
-    setState(newState);
-    return newState;
+  const newState = mutator();
+  setState(newState);
+  return newState;
 }
 
 export const GerglStateContext = React.createContext<
   GerglStateMachine
->(newGerglStateMachine(newGerglState(), (value: any) => {}));
+>(newGerglStateMachine(newGerglState(), (value: any) => { }));
 
 export function useGerglContext() {
-    return useContext(GerglStateContext);
+  return useContext(GerglStateContext);
 }
